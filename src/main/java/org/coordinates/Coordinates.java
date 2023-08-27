2bed20890c12954181c2ff9e693524fc76665461
MaledictYtb <villemurclement@gmail.com>
2023-08-27 20:00:17 +0200

    Some cleaning

--- src/main/java/org/coordinates/Coordinates.java
+++ src/main/java/org/coordinates/Coordinates.java
@@ -1,4 +1,4 @@
-package org.coordinates_toggle;
+package org.coordinates;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -12,26 +12,26 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.geysermc.floodgate.api.FloodgateApi;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class Coordinates_Toggle extends JavaPlugin implements Listener {
+public class Coordinates extends JavaPlugin implements Listener {
 
     private static Map<Player, Boolean> coordinatesEnabledMap = new HashMap<>();
 
     @Override
     public void onEnable() {
 
-        getLogger().info("Coordinates toggle for Java edition has been enabled");
+        getLogger().info("Coordinates plugin has been enabled");
 
         Bukkit.getPluginManager().registerEvents(this, this);
+
         BukkitRunnable task = new BukkitRunnable() {
             @Override
             public void run() {
                 for (Player player : Bukkit.getOnlinePlayers()) {
-                    if (coordinatesEnabledMap.get(player)) {
+                    if (coordinatesEnabledMap.get(player) == true) {
                         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.GREEN + "X : " + ChatColor.RESET + player.getLocation().getBlockX() + ChatColor.GREEN + " Y : " + ChatColor.RESET + player.getLocation().getBlockY() + ChatColor.GREEN + " Z : " + ChatColor.RESET + player.getLocation().getBlockZ()));
                     }
                 }
@@ -46,29 +46,36 @@ public class Coordinates_Toggle extends JavaPlugin implements Listener {
     public void onDisable() {
 
         coordinatesEnabledMap.clear();
+        getLogger().info("Coordinates plugin has been disabled");
 
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equals("coordinates")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                if (args.length == 0) {
-                    Boolean coordinatesEnabled = coordinatesEnabledMap.get(player);
-                    if (coordinatesEnabled) {
-                        coordinatesEnabledMap.put(player, false);
-                        player.sendMessage(ChatColor.RED + "Affichage des coordonnés désactivé. ");
-                    } else {
-                        coordinatesEnabledMap.put(player, true);
-                        player.sendMessage(ChatColor.GREEN + "Affichage des coordonnés activé. ");
-                    }
-                }
+
+            if (!(sender instanceof Player)) {
+                return false;
             }
-            return true;
+
+            if (args.length != 0) {
+                return false;
+            }
+
+            Player player = (Player) sender;
+            Boolean coordinatesEnabled = coordinatesEnabledMap.get(player);
+
+            if (coordinatesEnabled == true) {
+                coordinatesEnabledMap.put(player, false);
+                player.sendMessage(ChatColor.RED + "Coordinates disabled. ");
+            } else {
+                coordinatesEnabledMap.put(player, true);
+                player.sendMessage(ChatColor.GREEN + "Coordinates enabled. ");
+            }
         }
         return false;
     }
+
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -76,19 +83,8 @@ public class Coordinates_Toggle extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         String PlayerName = event.getPlayer().getName();
 
-        if(FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId()) == true) {
-
-            boolean coordinatesEnabled = true;
-            coordinatesEnabledMap.put(player, coordinatesEnabled);
-
-        }
-        else {
-
-            boolean coordinatesEnabled = false;
-            coordinatesEnabledMap.put(player, coordinatesEnabled);
-
-        }
-
+        boolean coordinatesEnabled = true;
+        coordinatesEnabledMap.put(player, coordinatesEnabled);
 
     }
 }
